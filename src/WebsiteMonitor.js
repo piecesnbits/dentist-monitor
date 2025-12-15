b5be64eae05b9d246d849b934f138210a982dd37
Pieces <piecesnbits@protonmail.com>
2025-12-15 13:48:20 +0100

    cc

--- src/WebsiteMonitor.js
+++ src/WebsiteMonitor.js
@@ -12,7 +12,14 @@ export default class WebsiteMonitor {
   }
 
   async fetchText() {
-    const response = await fetch(this.url, { timeout: 10_000 });
+
+    const response = await fetch(this.url, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+      'Accept-Language': 'en-US,en;q=0.5'
+    }
+    });
     const html = await response.text();
 
     const dom = new JSDOM(html);
